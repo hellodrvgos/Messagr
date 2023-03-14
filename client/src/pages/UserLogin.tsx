fa87e0370b5524643c67629583b0ee938e2a9449
Tudor <tudor@Dragoss-MacBook-Pro.local>
2023-03-14 21:21:40 +0200

    deleted socketio

---
+++ client/src/pages/UserLogin.tsx
@@ -0,0 +1,8 @@
+export default function UserLogin() {
+    return (
+        <div>
+            {/* Login with Password Component */}
+            {/* Login with Google Component */}
+        </div>
+    )
+}
