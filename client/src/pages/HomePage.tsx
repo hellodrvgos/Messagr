42c63b119e9fce9ff69cd3e5db5a877e8662fae2
Tudor <tudor@Dragoss-MacBook-Pro.local>
2023-03-14 12:13:35 +0200

    sockeio-local start

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ export default function HomePage() {
     return (
         <div>
             This is Homepage
+            Socket
         </div>
     )
 }
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { CSSProperties, useState } from "react";
 
 import valley from "../assets/valley.jpeg";
 import SignUpForm from "../components/AuthPage/SignUpForm";
-import LogInForm from "../components/AuthPage/LoginForm";
+import LogInForm from "../components/AuthPage/LogInForm";
 
 const AuthPage = () => {
   const [hasAccount, setHasAccount] = useState(false);
@@ -17,11 +17,11 @@ const AuthPage = () => {
         <div style={styles.formContainerStyle}>
           <div style={styles.titleStyle}>Pretty</div>
 
-          {hasAccount ? (
+          {/* {hasAccount ? (
             <LogInForm onHasNoAccount={() => setHasAccount(false)} />
           ) : (
             <SignUpForm onHasAccount={() => setHasAccount(true)} />
-          )}
+          )} */}
         </div>
       </div>
     </div>
