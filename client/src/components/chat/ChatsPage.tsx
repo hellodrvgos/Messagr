2136e58d77d9524c77190c102443add56b49a357
Tudor <tudor@Dragoss-MacBook-Pro.local>
2023-03-15 11:38:56 +0200

    chatengine light

--- client/src/components/chat/ChatsPage.tsx
+++ client/src/components/chat/ChatsPage.tsx
@@ -1,8 +1,8 @@
-// import {
-//   MultiChatSocket,
-//   MultiChatWindow,
-//   useMultiChatLogic,
-// } from 'react-chat-engine-advanced';
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from 'react-chat-engine-advanced';
 
 import { PrettyChatWindow } from "react-chat-engine-pretty"
 
@@ -12,22 +12,23 @@ type User = {
 }
 
 const ChatsPage = ({user}: {user: User}) => {
-  // const chatProps = useMultiChatLogic(
-  //   '0b414e0f-4fb4-4dc3-bae8-3541406bf83d',
-  //   user.username,
-  //   user.secret
-  // );
-  //   return <div style={{height: "100vh"}}>
-  //     <MultiChatWindow {...chatProps} style={{height: "100%"}}/>
-  //     <MultiChatSocket {...chatProps} />
-  //   </div>;
-return <div style={{height: "100vh"}}>
-  <PrettyChatWindow 
-  projectId='0b414e0f-4fb4-4dc3-bae8-3541406bf83d'
-  username={user.username}
-  secret={user.secret}
-  style={{height: "100%"}}
-/>
-</div>
+  const chatProps = useMultiChatLogic(
+    '0b414e0f-4fb4-4dc3-bae8-3541406bf83d',
+    user.username,
+    user.secret
+  );
+    return <div style={{height: "100vh"}}>
+      <MultiChatWindow {...chatProps} style={{height: "100%"}}/>
+      <MultiChatSocket {...chatProps} />
+    </div>;
+// return <div style={{height: "100vh"}}>
+//   <PrettyChatWindow 
+//   projectId='0b414e0f-4fb4-4dc3-bae8-3541406bf83d'
+//   username={user.username}
+//   secret={user.secret}
+//   style={{height: "100%"}}
+// />
+// <MultiChatSocket {...chatProps} />
+// </div>
   };
   export default ChatsPage;
