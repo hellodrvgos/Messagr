9798daddf47b567676223af2f169bf7657da4f55
Tudor <tudor@Dragoss-MacBook-Pro.local>
2023-03-18 21:50:56 +0200

    update ui

--- client/src/pages/Update.tsx
+++ client/src/pages/Update.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import UpdateProfileForm from "../components/user/update/updateForm/UpdateProfileForm";
 
 export default function Update() {
-  return <UpdateProfileForm />;
+  return (
+    <div className="update-page">
+        <UpdateProfileForm />;
+    </div>
+  )
 }
