6e5e444c492aa22d220b457be194121bb517dfd6
Helen <helen.hagos@integrify.io>
2023-03-19 13:49:58 +0100

    added collapsible table

--- client/src/components/user/userList/NewUserList.tsx
+++ client/src/components/user/userList/NewUserList.tsx
@@ -1,155 +1,86 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { TableSortLabel } from "@mui/material";
 
-import "../../../App.css"
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-//   carbs: number,
-//   protein: number,
-//   price: number,
-) {
-  return {
-    name,
-    calories,
-    fat,
-    // carbs,
-    // protein,
-    // price,
-    history: [
-      {
-        date: '2020-01-05',
-        customerId: '11091700',
-        amount: 3,
-      },
-      {
-        date: '2020-01-02',
-        customerId: 'Anonymous',
-        amount: 1,
-      },
-    ],
-  };
-}
-
-function Row(props: { row: ReturnType<typeof createData> }) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
-
-  return (
-    <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        {/* <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell> */}
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {/* {Math.round(historyRow.amount * row.price * 100) / 100} */}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
-}
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-//   createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-//   createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-// ];
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0,),
-    createData('Ice cream sandwich', 237, 9.0),
-    createData('Eclair', 262, 16.0),
-    createData('Cupcake', 305, 3.7),
-    createData('Gingerbread', 356, 16.0),
-  ];
+import "../../../App.css";
+import { useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "../../../redux/store";
+import { useEffect, useState } from "react";
+import fetchUsersData from "../../../redux/thunk/userThunk";
+import { useSelector } from "react-redux";
+import { getUserInformation } from "../../../redux/thunk/userInformation";
+import { userActions } from "../../../redux/slice/userSlice";
+import UserItem from "../userItem/UserItem";
 
 export default function CollapsibleTable() {
+  const [orderDirection, setOrderDirection] = useState<"asc" | "desc">("asc");
+  const userId = localStorage.getItem("id") || "{}";
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(fetchUsersData());
+  }, [dispatch, userId]);
+  const userList = useSelector((state: RootState) => state.users.userList);
+
+  useEffect(() => {
+    dispatch(getUserInformation());
+  }, [dispatch, userId]);
+  // sorting
+  const sortAscendingHandler = () => {
+    dispatch(userActions.sortAscending());
+    setOrderDirection(orderDirection === "asc" ? "desc" : "asc");
+  };
+
+  const sortDescendingHandler = () => {
+    dispatch(userActions.sortDescending());
+    setOrderDirection(orderDirection === "asc" ? "desc" : "asc");
+  };
+
+  const userInfo = useSelector(
+    (state: RootState) => state.userinformation.userInfo
+  );
+  const filteredUserList = userList.filter((user) => user._id !== userInfo._id);
   return (
-    <div className='userlist-page'>
-    <Box sx={{width: "600px", pt: 20, ml: 15}}>
-    <TableContainer component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            {/* <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <Row key={row.name} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-
-    </Box>
-
+    <div className="userlist-page">
+      <Box sx={{ width: "600px", pt: 20, ml: 15 }}>
+        <TableContainer component={Paper}>
+          <Table aria-label="collapsible table">
+            <TableHead>
+              <TableRow>
+                <TableCell />
+                <TableCell
+                  onClick={
+                    orderDirection === "asc"
+                      ? sortAscendingHandler
+                      : sortDescendingHandler
+                  }
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Name
+                  <TableSortLabel
+                    active={true}
+                    direction={orderDirection}
+                    sx={{ ml: 1 }}
+                  ></TableSortLabel>
+                </TableCell>
+                <TableCell align="right" sx={{ fontWeight: "bold" }}>
+                  Role
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredUserList.map((user, index) => (
+                <UserItem key={index} user={user} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </div>
   );
 }
